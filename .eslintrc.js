ded1266a585475ef8b1a5a506b9e333cdeac6e2c
silksofthesoul <silksofthesoul@yandex.ru>
2019-09-01 13:54:10 +0300

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,9 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-plusplus': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint',
