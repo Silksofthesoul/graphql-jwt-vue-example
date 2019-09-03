e6d3d9f73dff7fa7f437e00d5d9617c87978b5d6
silksofthesoul <silksofthesoul@yandex.ru>
2019-09-03 23:49:07 +0300

    if not have token no show "todo"

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">User</router-link> |
-      <router-link to="/Todo">Todo</router-link>
-    </div>
-    <router-view/>
+<div id="app">
+  <div id="nav">
+    <router-link to="/">User</router-link>&nbsp;
+    <router-link v-if="token" to="/Todo">Todo</router-link>
   </div>
+  <router-view />
+</div>
 </template>
+
+<script>
+import api from '@/api';
+export default {
+  name: 'App',
+  computed: {
+    token() {
+      return this.$store.getters['Auth/getToken'];
+    }
+  }
+};
+</script>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    padding: 30px;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
