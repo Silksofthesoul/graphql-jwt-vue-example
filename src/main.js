e03d5ad459a74a6f88103e2f763052d02282ac7d
silksofthesoul <silksofthesoul@yandex.ru>
2019-10-17 21:35:53 +0300

    add directives

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import VueHotkey from 'v-hotkey';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(VueHotkey);
 
 Vue.config.productionTip = false;
 
